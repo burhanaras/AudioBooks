b145d2631a7c11006837037896bb0a7ff318031f
Burhan <burhan.aras@turkcell.com.tr>
2019-10-25 10:35:22 +0300

    On Audio book detail screen, we have added title, image and back button.

--- app/src/main/java/com/burhan/audiobooksapp/presentation/ui/audiobookdetail/AudioBookDetailActivity.kt
+++ app/src/main/java/com/burhan/audiobooksapp/presentation/ui/audiobookdetail/AudioBookDetailActivity.kt
@@ -3,22 +3,53 @@ package com.burhan.audiobooksapp.presentation.ui.audiobookdetail
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import com.burhan.audiobooksapp.R
 import com.burhan.audiobooksapp.domain.model.AudioBook
+import com.burhan.audiobooksapp.presentation.core.extension.loadFromUrl
+import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_audio_book_detail.*
+import kotlin.math.abs
+
 
 class AudioBookDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_audio_book_detail)
+        setContentView(com.burhan.audiobooksapp.R.layout.activity_audio_book_detail)
         setSupportActionBar(toolbar)
+        setUpToolbar()
+
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        intent.getParcelableExtra<AudioBook>(ARG_AUDIO_BOOK)?.let { audioBook ->
+            tvAudioBookDetailTitle.text = audioBook.name
+            ivAudioBookDetailImage.loadFromUrl(audioBook.imageUrl)
+        }
+    }
+
+    private fun setUpToolbar() {
+        supportActionBar?.apply { setDisplayHomeAsUpEnabled(true) }
+        title = ""
+        app_bar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
+            tvAudioBookDetailTitle.alpha = abs(
+                verticalOffset / appBarLayout.totalScrollRange.toFloat()
+            )
+        })
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     companion object {
