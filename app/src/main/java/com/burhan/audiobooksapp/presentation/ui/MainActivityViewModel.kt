7d35ea48edc53edc5e1f23038dfea86fcf164b43
Burhan <burhan.aras@turkcell.com.tr>
2019-11-25 19:16:31 +0300

    We have introduced Mini Player Fab  button. (0)

--- app/src/main/java/com/burhan/audiobooksapp/presentation/ui/MainActivityViewModel.kt
+++ app/src/main/java/com/burhan/audiobooksapp/presentation/ui/MainActivityViewModel.kt
@@ -1,17 +1,30 @@
 package com.burhan.audiobooksapp.presentation.ui
 
+import android.app.Application
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.burhan.audiobooksapp.presentation.ui.player.service.PlayerService
 
 /**
  * Developed by tcbaras on 2019-10-24.
  */
-class MainActivityViewModel : ViewModel() {
+class MainActivityViewModel(private val app: Application) : AndroidViewModel(app) {
     var lastActiveFragmentTag: String? = null
 
-    var fabMiniPlayerVisibility : MutableLiveData<Boolean> = MutableLiveData()
+    var fabMiniPlayerVisibility: MutableLiveData<Boolean> = MutableLiveData()
 
     init {
         fabMiniPlayerVisibility.postValue(false)
+        val intentFilter = PlayerService.IntentFilter
+        LocalBroadcastManager.getInstance(app).registerReceiver(object : BroadcastReceiver() {
+            override fun onReceive(p0: Context?, p1: Intent?) {
+                if (fabMiniPlayerVisibility.value == false)
+                    fabMiniPlayerVisibility.postValue(true)
+            }
+        }, intentFilter)
     }
 }
