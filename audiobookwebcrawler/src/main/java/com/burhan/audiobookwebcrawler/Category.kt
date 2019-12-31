a52c1e1e10781a1ea2252eddaa17b5d368e0484f
Burhan <burhan.aras@turkcell.com.tr>
2019-12-31 14:38:56 +0300

    Finally we have redesigned our crawler.

---
+++ audiobookwebcrawler/src/main/java/com/burhan/audiobookwebcrawler/Category.kt
@@ -0,0 +1,12 @@
+package com.burhan.audiobookwebcrawler
+
+/**
+ * Developed by tcbaras on 2019-12-31.
+ */
+data class Category(
+    val id: String,
+    val name: String,
+    val imageUrl: String = "",
+    var audioBooks: List<AudioBook> = listOf(),
+    var order: String = "1000"
+)
--- audiobookwebcrawler/src/main/java/com/burhan/audiobookwebcrawler/MainActivity.kt
+++ audiobookwebcrawler/src/main/java/com/burhan/audiobookwebcrawler/MainActivity.kt
@@ -8,14 +8,10 @@ import android.view.MenuItem
 import android.webkit.WebViewClient
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 
@@ -67,141 +63,183 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        // startCrawling()
-        startCrawlingBestSellers()
-        startCrawlingNewReleases()
+        GlobalScope.launch {
+            fetchAudioBooks()
+        }
 
         webView.webViewClient = WebViewClient()
         webView.loadUrl(URL)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-
-    }
-
-    private fun startCrawlingBestSellers() {
-        val bestSellers = mutableListOf<AudioBook>()
-        var url =
-            "https://www.audible.com/adblbestsellers?pf_rd_p=e1595489-c152-4314-a5d7-ed60b7e2ecc8&pf_rd_r=2JMZJFS5KRBMPREVM17C&ref=a_adblbests_c5_pageNum_1&page="
-
-        var counter = 0
-        (1..5).forEach { i ->
-            val fullUrl = url + i
-            doCrawlingNew(fullUrl) {
-                bestSellers.addAll(it)
-                counter++
-                if (counter == 5) {
-                    Log.d("BURHANx", "We have ${bestSellers.size} bestsellers.")
-                }
+        fab.setOnClickListener {
+            GlobalScope.launch {
+                fetchAudioBooks()
             }
         }
+
     }
 
-    private fun startCrawlingNewReleases() {
+    private suspend fun fetchAudioBooks() {
+        coroutineScope {
+            val deferreds = listOf(
+                async {
+                    val newReleases = fetchNewReleases()
+                    Log.d(TAG, "We have ${newReleases.size} new releases.")
+                    saveToFireStore(Category("New Releases", "New Releases", "", newReleases, "1"))
+                },
+                async {
+                    val bestSellers = fetchBestSellers()
+                    Log.d(TAG, "We have ${bestSellers.size} best sellers.")
+                    saveToFireStore(Category("Best Sellers", "Best Sellers", "", bestSellers, "2"))
+                },
+                async {
+                    val favorites = fetchFavorites()
+                    Log.d(TAG, "We have ${favorites.size} favorites.")
+                    saveToFireStore(
+                        Category(
+                            "User Favorites",
+                            "User Favorites",
+                            "",
+                            favorites,
+                            "3"
+                        )
+                    )
+                }
+            )
+            deferreds.awaitAll()
+            Log.d(TAG, "Finally, we have crawled them all.")
+        }
+        Log.d(TAG, "And here we are.")
+    }
+
+    private suspend fun fetchNewReleases(): List<AudioBook> {
         val newReleases = mutableListOf<AudioBook>()
-        var url =
+        val url =
             "https://www.audible.com/newreleases?pf_rd_p=4151a082-617e-4be0-b9af-ab85a5887dc2&pf_rd_r=ZGQ1MZ5E55G6X3CH3D5D&ref=a_newreleas_c4_pageNum_1&publication_date=20191219-20191226&feature_six_browse-bin=9178177011&feature_five_browse-bin=9178163011&submitted=1&page="
 
-        var counter = 0
         (1..5).forEach { i ->
             val fullUrl = url + i
-            doCrawlingNew(fullUrl) {
-                newReleases.addAll(it)
-                counter++
-                if (counter == 5) {
-                    Log.d("BURHANx", "We have ${newReleases.size} new releases.")
-                }
-            }
+            val bookz = fetchBookDetail(fullUrl)
+            newReleases.addAll(bookz)
         }
+        return newReleases
     }
 
-    private fun doCrawlingNew(url: String, callback: (List<AudioBook>) -> Unit) {
-        GlobalScope.launch(Dispatchers.IO) {
+    private suspend fun fetchBestSellers(): List<AudioBook> {
+        val bestSellers = mutableListOf<AudioBook>()
+        val url =
+            "https://www.audible.com/adblbestsellers?pf_rd_p=e1595489-c152-4314-a5d7-ed60b7e2ecc8&pf_rd_r=2JMZJFS5KRBMPREVM17C&ref=a_adblbests_c5_pageNum_1&page="
 
-            val audioBooks = mutableListOf<AudioBook>()
+        (1..5).forEach { i ->
+            val fullUrl = url + i
+            val bookz = fetchBookDetail(fullUrl)
+            bestSellers.addAll(bookz)
+        }
 
-            var document = Jsoup.connect(url).get()
-            document?.let {
-                val mp3sOnPage = document.select("button[data-mp3]")
-                mp3sOnPage.forEachIndexed { index, element ->
-                    element.attr("data-mp3")?.let { mp3 ->
-                        //                        Log.d("MainActivity", "$index. $mp3")
+        return bestSellers
+    }
 
-                        val parentNode =
-                            element.parentNode()?.parentNode()?.parentNode()?.parentNode()
-                        parentNode?.let { parentNode ->
-                            var text = (parentNode as Element).text()
-                            val by = text.indexOf(" By: ")
-                            val name = text.substring(0, by)
-                            text = text.substring(by + " By: ".length)
-                            val narratedBy = text.indexOf(" Narrated by: ")
-                            val author = text.substring(0, narratedBy).trim()
-                            text = text.substring(narratedBy + " Narrated by: ".length)
-                            val length = text.indexOf(" Length: ")
-                            val narrated = text.substring(0, length)
-                            text = text.substring(length + " Length: ".length)
-                            val overall = text.indexOf("Overall")
-                            val len = text.subSequence(0, overall)
-                            text = text.substring(overall + "Overall".length)
-                            val performance = text.indexOf(" Performance ")
-                            val overalll = text.substring(0, performance)
-                            text = text.substring(performance + " Performance ".length)
-                            val story = text.indexOf(" Story ")
-                            val perf = text.substring(0, story)
-                            text = text.substring(story + " Story ".length)
-                            val fiveStars = text.indexOf(" out of 5 stars ")
-                            text = text.substring(fiveStars + " out of 5 stars ".length)
-                            text = text.substring(text.indexOf(" ") + 1)
-                            var fourDots = text.indexOf(".... ")
-                            if (fourDots < 0) fourDots = text.length
-                            val description = text.substring(0, fourDots)
-                            val image = parentNode.getElementById("nojs_img_").attr("src")
+    private suspend fun fetchFavorites(): List<AudioBook> {
 
-                            val audioBook = AudioBook(name, author, image, mp3, description)
-                            audioBooks.add(audioBook)
-                        }
-                    }
-                }
-
-                callback(audioBooks)
+        val document = getDocument("https://www.audible.com/")
+        val scripts = document.getElementsByTag("script")
+        val books = mutableListOf<AudioBook>()
+        var bookIds: MutableList<String>
+        scripts.forEach { script ->
+            if (script.toString().contains("sourceAsins")) {
+                var index = script.toString().indexOf("sourceAsins")
+                index += 15
+                var text = script.toString().substring(index)
+                val endIndex = text.indexOf("\";")
+                text = text.substring(0, endIndex)
+                bookIds = text.split(",") as MutableList<String>
+                val bookz = fetchBookDetail(bookIds)
+                books.addAll(bookz)
 
             }
         }
+        return books
     }
 
-    private fun startCrawling() {
-        GlobalScope.launch(Dispatchers.IO) {
+    private suspend fun fetchBookDetail(bookIds: MutableList<String>): List<AudioBook> {
+        val books = mutableListOf<AudioBook>()
+        bookIds.forEach {
+            val bookz = fetchBookDetail("https://www.audible.com/search?keywords=$it")
+            books.addAll(bookz)
+        }
+        return books
+    }
 
-            categories.forEach { category ->
+    private suspend fun fetchBookDetail(url: String): List<AudioBook> {
 
-                val books: MutableList<AudioBook> = mutableListOf()
-                for (page in 0..20) {
-                    val bookCrawler = GlobalScope.async {
-                        val url = urlPart0 + category.value + urlPart1 + page
+        val audioBooks = mutableListOf<AudioBook>()
+        val document = getDocument(url)
+        document?.let {
+            val mp3sOnPage = document.select("button[data-mp3]")
+            mp3sOnPage.forEachIndexed { index, element ->
+                element.attr("data-mp3")?.let { mp3 ->
 
+                    val parentNode =
+                        element.parentNode()?.parentNode()?.parentNode()?.parentNode()
+                    parentNode?.let { parentNode ->
+                        var text = (parentNode as Element).text()
+                        val by = text.indexOf(" By: ")
+                        val name = text.substring(0, by)
+                        text = text.substring(by + " By: ".length)
+                        val narratedBy = text.indexOf(" Narrated by: ")
+                        val author = text.substring(0, narratedBy).trim()
+                        text = text.substring(narratedBy + " Narrated by: ".length)
+                        val length = text.indexOf(" Length: ")
+                        val narrated = text.substring(0, length)
+                        text = text.substring(length + " Length: ".length)
+                        val overall = text.indexOf("Overall")
+                        val len = text.subSequence(0, overall)
+                        text = text.substring(overall + "Overall".length)
+                        val performance = text.indexOf(" Performance ")
+                        val overalll = text.substring(0, performance)
+                        text = text.substring(performance + " Performance ".length)
+                        val story = text.indexOf(" Story ")
+                        val perf = text.substring(0, story)
+                        text = text.substring(story + " Story ".length)
+                        val fiveStars = text.indexOf(" out of 5 stars ")
+                        text = text.substring(fiveStars + " out of 5 stars ".length)
+                        text = text.substring(text.indexOf(" ") + 1)
+                        var fourDots = text.indexOf(".... ")
+                        if (fourDots < 0) fourDots = text.length
+                        val description = text.substring(0, fourDots)
+                        val image = parentNode.getElementById("nojs_img_").attr("src")
+
+                        val audioBook = AudioBook(name, author, image, mp3, description)
+                        audioBooks.add(audioBook)
                     }
-
                 }
+            }
+        }
 
+        return audioBooks
+    }
 
-                val batch = db.batch()
-                val collection = db.collection(category.key)
-                Log.d("RESULTx", "${category.key} ------------------------->")
-                books.forEachIndexed { index, audioBook ->
-                    Log.d("RESULTx", "${String.format("%3d", index)}. $audioBook")
-                    var data: HashMap<String, Any> = HashMap<String, Any>()
-                    data["name"] = audioBook.name
-                    data["author"] = audioBook.author
-                    data["imageUrl"] = audioBook.imageUrl
-                    data["mp3"] = audioBook.mp3
-                    data["description"] = audioBook.description
-                    batch.set(collection.document(), data)
-                }
+    private suspend fun getDocument(url: String) = withContext(Dispatchers.IO) {
+        Jsoup.connect(url).get()
+    }
+
+    private fun saveToFireStore(category: Category) {
+        GlobalScope.launch(Dispatchers.IO) {
+            val batch = db.batch()
+            val collection = db.collection(category.name)
+            Log.d(
+                TAG,
+                "${category.name} (${category.audioBooks.size} audiobooks) is gonna be saved to Firestore------------------------->"
+            )
+            category.audioBooks.forEachIndexed { index, audioBook ->
+                Log.d(TAG, "${String.format("%3d", index)}. $audioBook")
+                val data: HashMap<String, Any> = HashMap()
+                data["name"] = audioBook.name
+                data["author"] = audioBook.author
+                data["imageUrl"] = audioBook.imageUrl
+                data["mp3"] = audioBook.mp3
+                data["description"] = audioBook.description
+                batch.set(collection.document(), data)
+            }
 //                batch.commit() !! Be Careful
-
-            }
-
         }
     }
 
@@ -220,5 +258,9 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    companion object {
+        val TAG: String = MainActivity::class.java.simpleName
     }
 }
